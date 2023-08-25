e4b58caac9ba250a4496e9280a83aa97b15e2c7e
gabrielkgrdo <gabrielderoque@gmail.com>
2023-08-25 09:26:16 -0300

    lista de servidores implementada

--- src/app/componentes/servidor/lista-servidor/lista-servidor.component.ts
+++ src/app/componentes/servidor/lista-servidor/lista-servidor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Servidor } from 'src/app/modelos/servidor';
 
 @Component({
   selector: 'app-lista-servidor',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-servidor.component.css']
 })
 export class ListaServidorComponent implements OnInit {
+  
+  SERVIDOR_INFO: Servidor [] = [
+    {
+      id: 1,
+      nome: 'Kleyber Gabriel',
+      cpf: '123.456.789-10',
+      email: 'kleyber@mail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '25/08/2023'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Servidor>(this.SERVIDOR_INFO);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
