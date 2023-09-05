3b89d584f2ab0cd4c863a671e2d80b5dddd46e42
gabrielkgrdo <gabrielderoque@gmail.com>
2023-09-05 10:25:33 -0300

    html e css componente atualizar ocorrencia

--- src/app/componentes/ocorrencia/atualizar-ocorrencia/atualizar-ocorrencia.component.ts
+++ src/app/componentes/ocorrencia/atualizar-ocorrencia/atualizar-ocorrencia.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Ocorrencia } from 'src/app/modelos/chamado';
+import { Servidor } from 'src/app/modelos/servidor';
+import { Usuario } from 'src/app/modelos/usuario';
+import { OcorrenciaService } from 'src/app/services/ocorrencia.service';
+import { ServidorService } from 'src/app/services/servidor.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-atualizar-ocorrencia',
@@ -7,9 +16,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtualizarOcorrenciaComponent implements OnInit {
 
-  constructor() { }
+  ocorrencia: Ocorrencia ={
+    prioridade: '',
+    status: '',
+    titulo: '',
+    descricaoOcorrencia: '',
+    servidor: '',
+    usuario: '',
+    nomeUsuario: '',
+    nomeServidor: '',
+  }
+
+  usuarios: Usuario[] = []
+  servidores: Servidor[] = []
+
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  titulo: FormControl = new FormControl(null, [Validators.required])
+  descricao: FormControl = new FormControl(null, [Validators.required])
+  servidor: FormControl = new FormControl(null, [Validators.required])
+  usuario: FormControl = new FormControl(null, [Validators.required])
+
+  constructor(
+    private servicoOcorrencia: OcorrenciaService,
+    private servicoUsuario: UsuarioService,
+    private servicoServidor: ServidorService,
+    private alerta: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.findAllUsuarios();
+    this.findAllServidores();
+  }
+
+  create(): void{
+    this.servicoOcorrencia.create(this.ocorrencia).subscribe(resposta => {
+      this.alerta.success('Ocorrencia criada com sucesso!', 'Aviso:');
+      this.router.navigate(['ocorrencias']);
+    }, ex => {
+      this.alerta.error(ex.error.error);
+    })
+  }
+
+  findAllUsuarios(): void{
+    this.servicoUsuario.findAll().subscribe(resposta => {
+      this.usuarios = resposta;
+    })  
+  }
+
+  findAllServidores(): void{
+    this.servicoServidor.findAll().subscribe(resposta => {
+      this.servidores = resposta;
+    })
+  }  
+
+  validaCampos(): boolean {
+    return this.titulo.valid && this.prioridade.valid && this.status.valid &&
+    this.servidor.valid && this.usuario.valid && this.descricao.valid
   }
 
 }
+
