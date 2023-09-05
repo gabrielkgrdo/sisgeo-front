8953a8013ba9285ed96522f45f27d6b386837188
gabrielkgrdo <gabrielderoque@gmail.com>
2023-09-05 09:25:06 -0300

    validação de formulário para criação de ocorrencia

--- src/app/componentes/ocorrencia/criar-ocorrencia/criar-ocorrencia.component.ts
+++ src/app/componentes/ocorrencia/criar-ocorrencia/criar-ocorrencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-ocorrencia',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarOcorrenciaComponent implements OnInit {
 
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  titulo: FormControl = new FormControl(null, [Validators.required])
+  descricao: FormControl = new FormControl(null, [Validators.required])
+  servidor: FormControl = new FormControl(null, [Validators.required])
+  usuario: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.titulo.valid && this.prioridade.valid && this.status.valid &&
+    this.servidor.valid && this.usuario.valid && this.descricao.valid
   }
 
 }
