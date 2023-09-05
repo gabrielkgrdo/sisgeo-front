05592588635cc1d20064b40353363e494d4865b6
gabrielkgrdo <gabrielderoque@gmail.com>
2023-09-05 11:34:55 -0300

    componente leitura de ocorrencia finalizado

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['ocorrencias/read/1'])
+    this.router.navigate(['home'])
   }
 
   logout() {
--- src/app/componentes/ocorrencia/leitura-ocorrencia/leitura-ocorrencia.component.ts
+++ src/app/componentes/ocorrencia/leitura-ocorrencia/leitura-ocorrencia.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Ocorrencia } from 'src/app/modelos/chamado';
+import { OcorrenciaService } from 'src/app/services/ocorrencia.service';
 
 @Component({
   selector: 'app-leitura-ocorrencia',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeituraOcorrenciaComponent implements OnInit {
 
-  constructor() { }
+  ocorrencia: Ocorrencia ={
+    prioridade: '',
+    status: '',
+    titulo: '',
+    descricaoOcorrencia: '',
+    servidor: '',
+    usuario: '',
+    nomeUsuario: '',
+    nomeServidor: '',
+  }
+
+  constructor(
+    private servicoOcorrencia: OcorrenciaService,
+    private alerta: ToastrService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.ocorrencia.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.servicoOcorrencia.findById(this.ocorrencia.id).subscribe(resposta => {
+      this.ocorrencia = resposta;
+    }, ex => {
+      this.alerta.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): string{
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string{
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
   }
 
 }
