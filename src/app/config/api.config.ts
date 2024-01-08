2a8e25f2636b42c3205988955c968d43f13eda9d
gabrielkgrdo <gabrielderoque@gmail.com>
2024-01-08 00:33:33 -0300

    mudando url api para railway

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
 
-    //baseUrl : 'https://sisgeo-production.up.railway.app'
-    baseUrl : 'http://localhost:8080'
+    baseUrl : 'https://sisgeo-production.up.railway.app'
+    //baseUrl : 'http://localhost:8080'
 }
